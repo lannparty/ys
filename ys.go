5766d57e416bb8c71861b4d8282bc2546dfa00be
lannparty <luanqtruong@gmail.com>
2020-05-20 14:28:25 -0700

    appendingitem

--- ys.go
+++ ys.go
@@ -8,30 +8,30 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-type subset map[interface{}]interface{}
+type subset map[string]interface{}
 
-func appendToMap(target subset, item subset) {
-        pointer := target
+func appendToMap(targetMap subset, appendingItemKey string, appendingItemValue interface{}) {
+        pointer := targetMap
         for len(pointer) != 0 {
                 for key, _ := range pointer {
                         pointer = pointer[key].(subset)
                 }
         }
-        fmt.Println(item)
+        pointer[appendingItemKey] = appendingItemValue
 }
 
-//func searchMap(child subset, cache subset, target interface{}) {
-//        for key, _ := range child {
-//                nextCache := cache
-//                appendToMap(nextCache, key.(subset))
-//
-//                switch nextChild := child[key].(type) {
-//                case string:
-//                case map[interface{}]interface{}:
-//                        searchMap(nextChild, nextCache, target)
-//                }
-//        }
-//}
+func searchMap(child subset, cache subset, target interface{}) {
+        for key, _ := range child {
+                nextCache := cache
+                appendToMap(nextCache, key.(subset))
+
+                switch nextChild := child[key].(type) {
+                case string:
+                case map[interface{}]interface{}:
+                        searchMap(nextChild, nextCache, target)
+                }
+        }
+}
 
 func main() {
 
@@ -45,10 +45,20 @@ func main() {
 	if err != nil {
 		log.Fatalf("cannot unmarshal data: %v", err)
 	}
+	testMap := subset{}
+        testMap2 := subset{}
+        testMap3 := subset{}
+        testMap4 := subset{}
+        testMap5 := subset{}
+        testMap6 := subset{}
+        testMap5["key5"] = testMap6
+        testMap4["key4"] = testMap5
 
-        cache := make(map[interface{}]interface{})
-        cache2 := make(map[interface{}]interface{})
-        cache["key3"] = cache2
-        searchMap(m, cache, "gid-prod")
+        testMap2["key2"] = testMap3
+        testMap["key"] = testMap2
+
+        //searchMap(m, cache, "gid-prod")
+        appendToMap(testMap3, "testMap123", testMap4)
+	fmt.Println(testMap)
 
 }
